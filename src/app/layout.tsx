1225db0c8ce2691f6efd931e0882dfeb835cfc1d
jortegaruiz <jortegaruiz@gmail.com>
2024-06-03 23:50:30 -0300

    Nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,18 @@ import "./ui/global.css";
 import { raleway } from "./ui/fonts";
 import { NextUIProvider } from "@nextui-org/react";
 import AdviserContextProvider from "./context/AdviserContext";
+import Nav from "./ui/Nav";
 
 export const metadata: Metadata = {
   title: "Cásate Conmigo",
   description: "Joyeria de anillos de boda en Ecuador",
 };
+
+const items = [
+  { name: "Tienda", link: "/shop" },
+  { name: "Conoce más", link: "/mas" },
+  { name: "Nosotros", link: "/nosotros" },
+];
 
 export default function RootLayout({
   children,
@@ -18,7 +25,12 @@ export default function RootLayout({
     <html lang="es">
       <body className={`${raleway.className} antialiased`}>
         <AdviserContextProvider>
-          <NextUIProvider>{children}</NextUIProvider>
+          <NextUIProvider>
+            <header>
+              <Nav elements={items} />
+            </header>
+            {children}
+          </NextUIProvider>
         </AdviserContextProvider>
       </body>
     </html>
